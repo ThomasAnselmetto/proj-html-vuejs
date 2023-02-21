6173512a197b9b2b0ea567cc3b5cf6ff7209777c
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-02-21 17:21:57 +0100

    added with vue.js icons links and footer items

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,80 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {};
+    return {
+      links: [
+        {
+          logo: "./IMG/MasterStudy-1.svg",
+        },
+        {
+          text: "Courses",
+        },
+        {
+          text: "Course Formats",
+        },
+        {
+          text: "Add Course",
+        },
+        {
+          text: "Pages",
+        },
+        {
+          text: "Demos",
+        },
+        {},
+      ],
+      items: [
+        {
+          text: "Blog",
+        },
+        {
+          text: "Home",
+        },
+        {
+          text: "Shortcodes",
+        },
+
+        {
+          text: "Courses",
+        },
+        {
+          text: "Membership",
+        },
+
+        {
+          text: "Typography",
+        },
+      ],
+
+      socials: [
+        {
+          name: "Linkedin",
+          logo: "fa-brands fa-linkedin",
+          CEO: "Ryan Roslansky",
+        },
+        {
+          name: "Instagram",
+          logo: "fa-brands fa-instagram",
+          CEO: "Kevin Systrom",
+        },
+        {
+          name: "Facebook",
+          logo: "fa-brands fa-facebook",
+          CEO: "Mark Zuckerberg",
+        },
+        {
+          name: "Twitter",
+          logo: "fa-brands fa-twitter",
+          CEO: "Elon Musk",
+        },
+      ],
+    };
   },
+  // computed:{
+
+  // }
+  // methods:{
+
+  // },
 });
 app.mount("#app");
