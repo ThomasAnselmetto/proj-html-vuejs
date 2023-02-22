1de669f7f892a85aff79bf56887672974565fddc
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-02-22 14:57:52 +0100

    fixed some margin and padding

--- JS/count.js
+++ JS/count.js
@@ -3,20 +3,18 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const countDownDate = new Date("Mar 4, 2023 00:00:00").getTime();
+const counter = setInterval(boleanersCountDown, 1000);
+const countDownDate = new Date("Feb 23, 2023 00:00:00").getTime();
 console.log(countDownDate);
 
 let totalSeconds = 0;
 boleanersCountDown();
 
-const counter = setInterval(boleanersCountDown, 1000);
-
 function boleanersCountDown() {
   const now = new Date().getTime();
   let gapDiDate = countDownDate - now;
-
-  ++totalSeconds;
   console.log(gapDiDate);
+  ++totalSeconds;
   let days = Math.floor(gapDiDate / (1000 * 60 * 60 * 24));
   daysEl.innerHTML = days < 10 ? "0" + days : days;
 
@@ -30,6 +28,10 @@ function boleanersCountDown() {
 
   let seconds = Math.floor((gapDiDate % (1000 * 60)) / 1000);
   secondsEl.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+  // console.log(days);
+  // console.log(hours);
+  // console.log(minutes);
+  // console.log(seconds);
 
   if (gapDiDate < 0) {
     clearInterval();
